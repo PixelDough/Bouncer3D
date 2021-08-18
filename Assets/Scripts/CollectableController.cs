6c0ea78308927031f43d2c4589855a054bcee5ad
PixelDough <alworrell19@gmail.com>
2021-08-18 00:23:33 -0500

    Added seashell

---
+++ Assets/Scripts/CollectableController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace PixelDough.Bouncer
+{
+    public class CollectableController : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem collectParticleSystem;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.attachedRigidbody) return;
+            if (other.attachedRigidbody.CompareTag("Player"))
+            {
+                collectParticleSystem.transform.parent = null;
+                collectParticleSystem.Play();
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
