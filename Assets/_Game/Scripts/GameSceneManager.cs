507518ee07a3c5e73b9e2b4a5d7ddc70cae0314e
PixelDough <alworrell19@gmail.com>
2024-09-12 20:28:19 -0500

    Fixed scene loading timescale

--- Assets/_Game/Scripts/GameSceneManager.cs
+++ Assets/_Game/Scripts/GameSceneManager.cs
@@ -26,6 +26,7 @@ namespace PixelDough.Bouncer
         private static IEnumerator<float> C_LoadScene(SceneDependencySettingsSO sceneDependencySettings)
         {
             _isChangingScenes = true;
+            Time.timeScale = 0f;
             
             // Fade out
             int fadeID = GameManager.Instance.screenFadeController.FadeToBlack().uniqueId;
@@ -40,6 +41,7 @@ namespace PixelDough.Bouncer
 
             // Wait a buffer time for any start methods to process and lag the game
             yield return Timing.WaitForSeconds(1f);
+            Time.timeScale = 1f;
 
             // Fade from black, and set the isChangingScenes variable to false on complete
             GameManager.Instance.screenFadeController.FadeFromBlack().setOnComplete(() => _isChangingScenes = false);
