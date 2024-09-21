8995bc18e7fa7593289f14b80f04288f7fe353e6
PixelDough <alworrell19@gmail.com>
2024-09-21 13:12:13 -0500

    Set spike balls to kill the player

--- Assets/_Game/Scripts/KillZone.cs
+++ Assets/_Game/Scripts/KillZone.cs
@@ -9,11 +9,18 @@ public class KillZone : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (!other.attachedRigidbody) return;
+        if (!other.attachedRigidbody.CompareTag("Player")) return;
         
-        if (other.attachedRigidbody.CompareTag("Player"))
-        {
-            PlayerController playerController = other.attachedRigidbody.GetComponent<PlayerController>();
-            playerController.Kill();
-        }
+        PlayerController playerController = other.attachedRigidbody.GetComponent<PlayerController>();
+        playerController.Kill();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (!other.rigidbody) return;
+        if (!other.rigidbody.CompareTag("Player")) return;
+        
+        PlayerController playerController = other.rigidbody.GetComponent<PlayerController>();
+        playerController.Kill();
     }
 }
